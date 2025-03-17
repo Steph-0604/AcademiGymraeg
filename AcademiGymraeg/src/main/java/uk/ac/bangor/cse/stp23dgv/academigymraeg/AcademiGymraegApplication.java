c3713b3261eeae6ffa3693c2f49adf9d86142c00
x <x@147.143.137.93>
2025-03-17 13:52:56 +0000

    Initial commit.

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/AcademiGymraegApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiGymraegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiGymraegApplication.class, args);
+	}
+
+}
---
+++ AcademiGymraeg/src/test/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/AcademiGymraegApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiGymraegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
