3f46ef887e9196646d558b7ba439ffb779d97443
x <x@147.143.137.93>
2025-03-17 16:07:29 +0000

    User Class with user details interface

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/User.java
@@ -0,0 +1,28 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
