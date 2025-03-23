07260c33d8a1b9f7703012d5cf347ec16894ae73
Grishma <grishma1229@gmail.com>
2025-03-23 22:22:17 +0000

    added variables and types

--- AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Question.java
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Question.java
@@ -1,5 +1,10 @@
 package uk.ac.bangor.cse.stp23dgv.academigymraeg.model;
 
+
 public class Question {
+	private int Id;
+	private String questionText;
+	private String questionType; //Gender, Meaning, Translation
+	private String correctAnswer;
 
 }
