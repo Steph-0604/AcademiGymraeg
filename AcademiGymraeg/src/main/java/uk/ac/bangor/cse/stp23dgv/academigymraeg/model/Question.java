b82ae62b6faf87cbe59d8e78cc191238409b7cf3
Grishma <grishma1229@gmail.com>
2025-03-23 22:51:13 +0000

    added englishNoun and welshNoun in constructor

--- AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Question.java
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Question.java
@@ -15,7 +15,7 @@ public class Question {
 	private String questionType; //Gender, Meaning, Translation
 	private String correctAnswer;
 	
-	public Question(String questionText, String questionType, String correctAnswer) {
+	public Question(String englishNoun, String welshNoun, String questionType, String correctAnswer) {
         this.questionText = questionText;
         this.questionType = questionType;
         this.correctAnswer = correctAnswer;
