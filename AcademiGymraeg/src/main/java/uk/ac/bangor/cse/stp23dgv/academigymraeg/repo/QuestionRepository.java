3a4b762158af9d534b1c4abcf3933665a2356d3b
Grishma <grishma1229@gmail.com>
2025-03-31 14:29:42 +0100

    added question repo

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/repo/QuestionRepository.java
@@ -0,0 +1,9 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.model.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
