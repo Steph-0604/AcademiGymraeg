3948ed489eb634264323124a0dae03bbc59a88b8
Steph <x@147.143.137.105>
2025-03-31 13:22:54 +0100

    Username is an ID and cannot be null, blank, it has to be an email and size is max 255. Password cannot be null as well. Authority is granted if user is admin.

--- AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/User.java
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/User.java
@@ -1,28 +1,54 @@
 package uk.ac.bangor.cse.stp23dgv.academigymraeg.model;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User implements UserDetails {
+	@Id
+	@NotBlank
+	@NotNull
+	@Size(max=255)
+	@Email
+	@Column(nullable = false, updatable = false)
+	private String username;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private boolean admin = false;
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		List<GrantedAuthority>perms = new LinkedList<GrantedAuthority>();
+		perms.add(new SimpleGrantedAuthority("ROLE_USER"));
+		if(admin)
+			perms.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		
+		return perms;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return username;
 	}
 
 }
