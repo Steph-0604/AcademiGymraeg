75bf3ec9e41e0a18fcfdd530a6e6413b7e124bed
Doowhop01 <harry.aylward01@gmail.com>
2025-04-22 13:39:56 +0100

    Test Class

--- AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Test.java
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/model/Test.java
@@ -1,5 +1,94 @@
 package uk.ac.bangor.cse.stp23dgv.academigymraeg.model;
 
-public class Test {
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Represents a Test entity for the application
+ *
+ * @author Harry
+ */
+@Entity
+@Table(name = "tests")
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+    
+    @Column
+    private LocalDateTime dateCompleted;
+    
+    @Column
+    private Integer score;
+    
+    @Column(nullable = false)
+    private Boolean completed = false;
+    
+    @OneToMany(mappedBy = "test", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public LocalDateTime getDateCompleted() {
+		return dateCompleted;
+	}
+
+	public void setDateCompleted(LocalDateTime dateCompleted) {
+		this.dateCompleted = dateCompleted;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
 }
