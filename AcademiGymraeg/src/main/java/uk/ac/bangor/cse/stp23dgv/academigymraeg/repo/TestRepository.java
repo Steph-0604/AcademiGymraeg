f571b62a049d98aa520244604305906a66fd2d70
Doowhop01 <harry.aylward01@gmail.com>
2025-05-03 14:10:53 +0100

    Add files via upload

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/repo/TestRepository.java
@@ -0,0 +1,9 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.model.Test;
+
+public interface TestRepository extends JpaRepository<Test, Integer> {
+
+}
