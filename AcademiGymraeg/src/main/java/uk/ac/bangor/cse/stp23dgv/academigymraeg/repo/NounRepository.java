45291bd3fd84f36f4487125c5515e776caaa6b3b
Doowhop01 <harry.aylward01@gmail.com>
2025-05-03 21:07:22 +0100

    Add files via upload

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/repo/NounRepository.java
@@ -0,0 +1,11 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.model.Noun;
+
+public interface NounRepository extends JpaRepository<Noun, Integer> {
+
+
+}
+
