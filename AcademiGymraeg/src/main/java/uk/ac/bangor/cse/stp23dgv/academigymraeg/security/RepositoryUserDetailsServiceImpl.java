d71483ba08b823e5e0abb1eef43f5865963a239d
Steph <x@User.home>
2025-05-04 21:36:00 +0100

    Custom implementation of UserDetailsService: - Loads user details from the UserRepository based on the username - Throws UsernameNotFoundException if the user does not exist in the repository

---
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/security/RepositoryUserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package uk.ac.bangor.cse.stp23dgv.academigymraeg.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.model.User;
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.repo.UserRepository;
+
+@Component
+
+public class RepositoryUserDetailsServiceImpl implements UserDetailsService {
+
+		@Autowired
+		private UserRepository repo;
+		
+		@Override
+		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+			Optional<User> obj = repo.findById(username);
+			
+			if (obj.isEmpty())
+				throw new UsernameNotFoundException(username);
+			
+			return obj.get();
+		}
+
+}
