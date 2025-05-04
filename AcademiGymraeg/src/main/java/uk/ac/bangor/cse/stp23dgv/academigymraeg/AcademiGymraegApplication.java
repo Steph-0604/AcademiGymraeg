adc0b7bed5bdd8df5c4766d0b1fe4ef6c477159c
Steph <x@User.home>
2025-05-04 21:34:04 +0100

    Enables form-based login and logout functionality Configures custom UserDetailsService for authentication Implements password encoding for secure user credentials

--- AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/AcademiGymraegApplication.java
+++ AcademiGymraeg/src/main/java/uk/ac/bangor/cse/stp23dgv/academigymraeg/AcademiGymraegApplication.java
@@ -1,13 +1,41 @@
 package uk.ac.bangor.cse.stp23dgv.academigymraeg;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import uk.ac.bangor.cse.stp23dgv.academigymraeg.security.RepositoryUserDetailsServiceImpl;
+
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 
 @SpringBootApplication
 public class AcademiGymraegApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AcademiGymraegApplication.class, args);
-	}
+    @Autowired
+    private RepositoryUserDetailsServiceImpl uds; 
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiGymraegApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securitySetup(HttpSecurity http) throws Exception {
+        return http.userDetailsService(uds).formLogin(form -> form.loginProcessingUrl("/login"))
+                .logout(lo -> lo.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))).build();
+    }
 
 }
